73d29974c804ebe5032f83154afc5479aa38e0fd
taka0707 <yukken777@gmail.com>
2020-07-17 19:27:46 +0900

    done-rubocop
    - rubocop実行済

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
 
   has_many :items
 
-  VALID_PASSWORD = /(?=.*?[a-z])(?=.*?\d)[a-z\d]/i
-  VALID_NAME = /\A[ぁ-んァ-ン一-龥]/
-  VALID_NAME_KANA = /\A[ァ-ヶー－]+\z/
-  VALID_EMAIL = /[a-z\d]+@[a-z\d]+/i
+  VALID_PASSWORD = /(?=.*?[a-z])(?=.*?\d)[a-z\d]/i.freeze
+  VALID_NAME = /\A[ぁ-んァ-ン一-龥]/.freeze
+  VALID_NAME_KANA = /\A[ァ-ヶー－]+\z/.freeze
+  VALID_EMAIL = /[a-z\d]+@[a-z\d]+/i.freeze
 
   validates :nickname, :email, :password, :family_name, :first_name, :family_name_kana,
             :first_name_kana, :birth_date, presence: true
@@ -16,5 +16,4 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true
   validates :family_name, :first_name, format: { with: VALID_NAME }
   validates :family_name_kana, :first_name_kana, format: { with: VALID_NAME_KANA }
-  
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,13 @@
 FactoryBot.define do
-
   factory :user do
-    nickname              {"abe"}
-    password              {"aaa000"}
-    password_confirmation {"aaa000"}
-    sequence(:email) {Faker::Internet.email}
-    family_name           {"あ亜ア"}
-    first_name            {"あ亜ア"}
-    family_name_kana      {"アベ"}
-    first_name_kana       {"アベ"}
-    birth_date            {"1900-10-10"}
+    nickname              { 'abe' }
+    password              { 'aaa000' }
+    password_confirmation { 'aaa000' }
+    sequence(:email) { Faker::Internet.email }
+    family_name           { 'あ亜ア' }
+    first_name            { 'あ亜ア' }
+    family_name_kana      { 'アベ' }
+    first_name_kana       { 'アベ' }
+    birth_date            { '1900-10-10' }
   end
-
 end
