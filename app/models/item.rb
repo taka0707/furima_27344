f0ab37cfbc0eba9cc2e32140de2ded7a065b352c
taka0707 <yukken777@gmail.com>
2020-07-18 11:26:28 +0900

    add-active_hash
    - postage_payer/prefecture/preparation_dayのactivehash作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,12 +16,18 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :preparation_day
 
   validates :category, presence: true
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
+    validates :postage_payer_id
+    validates :prefecture_id
+    validates :preparation_day_id
 
   end
 end
