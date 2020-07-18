1f5c678e3ab652f15fc8be44505b4141baac19d1
taka0707 <yukken777@gmail.com>
2020-07-18 20:58:31 +0900

    edit-item_files
    - item_controllerのbefore_action修正
    - item.rbのvalidation修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,20 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  validates :name, presence: true
-  validates :introduction, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :postage_payer_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :preparation_day_id, presence: true
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :category_id
+    validates :condition_id
+    validates :postage_payer_id
+    validates :prefecture_id
+    validates :preparation_day_id
+    validates :image
+  end
   validates :price, numericality: { greater_than_or_equal_to: 300,
                                     less_than_or_equal_to: 9_999_999,
                                     only_integer: true }
-  validates :image, presence: true
-
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
