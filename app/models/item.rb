f198486dd1dbf253b1dbc43ccc3f13b77dd2f516
taka0707 <yukken777@gmail.com>
2020-07-18 10:59:32 +0900

    add-condition_model
    - conditionアクティブハッシュの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,27 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :postage_payer_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :preparation_day_id, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, 
+                                    less_than_or_equal_to: 9999999,
+                                    only_integer: true }
+  validates :image,presence: true
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
 
-  validates :title, :text, :genre, presence: true
+  validates :category, presence: true
 
   with_options numericality: { other_than: 1 } do
-    validates :genre_id
+    validates :category_id
+    validates :condition_id
+
   end
 end
