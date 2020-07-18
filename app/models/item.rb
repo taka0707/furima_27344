a6158ac14d7baf80f7362affca3e46ade78a9394
taka0707 <yukken777@gmail.com>
2020-07-18 10:41:40 +0900

    add-category_model
    - categoryアクティブハッシュ作成
    - itemのnewルーティング設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+
+  validates :title, :text, :genre, presence: true
+
+  with_options numericality: { other_than: 1 } do
+    validates :genre_id
+  end
 end
