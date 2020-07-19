40cb00fb04d44fe4765298e21146aa23008c8943
taka0707 <yukken777@gmail.com>
2020-07-19 21:35:59 +0900

    edit-edit.html&item_controller
    - 商品編集画面の各項目の表示追記
    - item_controllerのprivateアクションにset_itemとmove_to_indexを追加し、before_actionに設定
    - 商品詳細画面に編集画面へのパス追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_index, only: [:edit]
+  
   def index
-    @items = Item.includes(:sending_destination)
+    @items = Item.includes(:sending_destination, :user)
   end
 
   def new
@@ -19,15 +21,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
-  def update
-    @item = Item.find(params[:id])
-    if @item.save
+  def update 
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
@@ -39,5 +39,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :introduction, :image, :category_id, :condition_id, :postage_payer_id,
                                  :prefecture_id, :preparation_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index if current_user.id != @item.user_id
   end
 end
