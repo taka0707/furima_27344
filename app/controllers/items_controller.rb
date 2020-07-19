d5af44e56639b43f49297d3ed027e1fe6a7199e7
taka0707 <yukken777@gmail.com>
2020-07-19 11:31:54 +0900

    add-association
    - itemモデルにsending_destinationとのアソシエーション定義
    - item_controllerのindexアクション修正
    - index画面にsoldoutの分岐追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.all
+    @items = Item.includes(:sending_destination)
   end
 
   def new
