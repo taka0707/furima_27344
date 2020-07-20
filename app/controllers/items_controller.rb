44c145ad776a6ad24a79c9a9494ccfd484953af3
taka0707 <yukken777@gmail.com>
2020-07-20 11:49:58 +0900

    edit-items_controller
    - items_controllerにdestroyアクション追加
    - itemsのdestroyルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -32,6 +32,10 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
   end
 
   private
