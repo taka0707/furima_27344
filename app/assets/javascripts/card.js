48b3ddc22d3f0d9360404a4ebe31422231f71781
taka0707 <yukken777@gmail.com>
2020-07-28 11:07:15 +0900

    edit

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,7 +1,7 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
-
+  console.log("test")
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
